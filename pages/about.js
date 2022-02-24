13cb874dd2136aceabf0a247b718595f88fbba04
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-02-24 13:46:59 -0500

    jukebox

--- pages/about.js
+++ pages/about.js
@@ -2,7 +2,8 @@ import React from "react";
 import Page from "../layouts/page";
 import Box from "../components/shared/box";
 import LikesContainer from "../components/about/likesContainer";
-export default function About() {
+import MusicBox from "../components/about/musicBox";
+export default function About({ songInfo: { artist, imgUrl, album, name ,lastFmUrl } }) {
   return (
     <Page>
       <Box title="About Me">
@@ -21,12 +22,41 @@ export default function About() {
           <li>hello</li>
         </LikesContainer>
       </Box>
-      <Box title="What I'm Listening To"></Box>
+      <Box title="What I'm Listening To Right Now">
+        <MusicBox
+          name={name}
+          artist={artist}
+          imgUrl={imgUrl}
+          album={album}
+          lastFmUrl={lastFmUrl}
+        ></MusicBox>
+      </Box>
       <Box title="Credits">
         <h3>
-          Built using Next.js, Styled Components, Framer Motion, MDX and Netlify.
+          Built using Next.js, Styled Components, Framer Motion, MDX and
+          Netlify.
         </h3>
       </Box>
     </Page>
   );
+}
+export async function getStaticProps() {
+  const apiKey = process.env.LAST_FM_API_KEY;
+  const url = `https://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=andrewchen2004&api_key=${apiKey}&format=json`;
+  const req = await fetch(url);
+  const res = await req.json();
+  const firstSong = res.recenttracks.track[0];
+  const artist = firstSong.artist["#text"];
+
+
+  const imgUrl = firstSong.image[firstSong.image.length - 2]["#text"];
+  const album = firstSong.album["#text"];
+  const name = firstSong.name;
+  const lastFmUrl = firstSong.url;
+  const songInfo = { artist, imgUrl, album, name, lastFmUrl };
+  return {
+    props: {
+      songInfo,
+    },
+  };
 }
