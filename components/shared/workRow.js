a7d2a75b72de9c8b77b48c86c50eb68e2a235b09
Andrew Chen <andrew.chen.anyuan@gmail.com>
2022-02-23 20:34:19 -0500

    made home page use dynamic data rather than hardcoded UI

--- components/shared/workRow.js
+++ components/shared/workRow.js
@@ -1,23 +1,28 @@
-import React from 'react'
-import styled from 'styled-components'
-import TextStyles from '../../util/textStyles'
+import Link from "next/link";
+import React from "react";
+import styled from "styled-components";
+import TextStyles from "../../util/textStyles";
 const Row = styled.div`
-    display: flex;
-    justify-content: space-between;
-    align-items: end;
-    h2 {
-        ${TextStyles.oversized}
-    }
-    h3 {
-        ${TextStyles.heading}
-        color: white;
-    }
-`
-export default function WorkRow({projectName, year}) {
+  display: flex;
+  justify-content: space-between;
+  align-items: end;
+  h2 {
+    ${TextStyles.oversized}
+  }
+  h3 {
+    ${TextStyles.heading}
+    color: white;
+  }
+`;
+export default function WorkRow({ projectName, year, link }) {
   return (
-    <Row>
-        <h2>{projectName}</h2>
-        <h3>{year}</h3>
-    </Row>
-  )
+    <Link href={`/work/${link}`}>
+      <a>
+        <Row>
+          <h2>{projectName}</h2>
+          <h3>{year}</h3>
+        </Row>
+      </a>
+    </Link>
+  );
 }
